962d1fa2549fb58217899944f346e483045df804
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2018-03-19 16:21:17 -0700

    Bump version: 0.2.0 → 0.2.1

--- shell_utils/__init__.py
+++ shell_utils/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Stephan Fitzpatrick"""
 __email__ = 'knowsuchagency@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from shell_utils.shell_utils import shell, env, path, cd, quiet
 
