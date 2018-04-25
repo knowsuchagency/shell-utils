e13157a790d3150d637ba4fa56c6cd0a890e2906
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2018-04-24 18:21:54 -0700

    Publishing to gh-pages (automated)

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '__version__ = &#39;0.4.0&#39;',
+    VERSION: '0.5.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
